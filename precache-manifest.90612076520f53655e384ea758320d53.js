a6743d368e365ab6984355dc8e6fdbfcdf5de0db
iamsubroto <smartcoder500@gmail.com>
2018-12-25 08:15:17 -0800

    Updates

---
+++ precache-manifest.90612076520f53655e384ea758320d53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c738b07d86f4db702dc0",
+    "url": "/reactTodo/static/js/runtime~main.c738b07d.js"
+  },
+  {
+    "revision": "54deb07310f361b1a5fa",
+    "url": "/reactTodo/static/js/main.54deb073.chunk.js"
+  },
+  {
+    "revision": "b6eceac682ec1965e2ea",
+    "url": "/reactTodo/static/js/1.b6eceac6.chunk.js"
+  },
+  {
+    "revision": "54deb07310f361b1a5fa",
+    "url": "/reactTodo/static/css/main.16526a11.chunk.css"
+  },
+  {
+    "revision": "b1981884dff6e20fe7e9ef4074259b5b",
+    "url": "/reactTodo/index.html"
+  }
+];
